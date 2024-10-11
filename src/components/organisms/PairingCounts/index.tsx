f603525dfa68b3749eeae0ef2712d3b5d1f4ffa9
KM0912 <61630275+KM0912@users.noreply.github.com>
2024-10-11 09:02:35 +0900

    ペアになった回数を表で表示するように変更

--- src/components/organisms/PairingCounts/index.tsx
+++ src/components/organisms/PairingCounts/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { Space, Typography } from "antd";
+import React, { useEffect, useState } from "react";
+import { Table, Typography } from "antd";
 import { PairingCounts as PairingCountsType } from "../../../types";
+import { usePlayers } from "../../../context/PlayersContext";
 
 const { Text } = Typography;
 
@@ -8,14 +9,64 @@ type Props = {
   pairingCounts: PairingCountsType;
 };
 
+type DataSource = {
+  userId1: number;
+  userId2: number;
+  count: number;
+}[];
+
 const PairingCounts: React.FC<Props> = ({ pairingCounts }) => {
-  const pairingCountsString = JSON.stringify(pairingCounts);
+  const { players } = usePlayers();
+  const [dataSource, setDataSource] = useState<DataSource>([]);
+
+  useEffect(() => {
+    const newDataSource: DataSource = [];
+    const playerIds = players.map((player) => player.id);
+
+    playerIds.forEach((userId1) => {
+      playerIds.forEach((userId2) => {
+        if (userId1 !== userId2) {
+          const count =
+            userId1 < userId2
+              ? pairingCounts[userId1]?.[userId2] || 0
+              : pairingCounts[userId2]?.[userId1] || 0;
+          newDataSource.push({ userId1, userId2, count });
+        }
+      });
+    });
+
+    newDataSource.sort((a, b) => {
+      if (a.userId1 === b.userId1) {
+        return a.userId2 - b.userId2;
+      }
+      return a.userId1 - b.userId1;
+    });
+
+    setDataSource(newDataSource);
+  }, [pairingCounts, players]);
+
+  const columns = [
+    {
+      title: "ユーザー1",
+      dataIndex: "userId1",
+      key: "userId1",
+    },
+    {
+      title: "ユーザー2",
+      dataIndex: "userId2",
+      key: "userId2",
+    },
+    {
+      title: "カウント",
+      dataIndex: "count",
+      key: "count",
+    },
+  ];
 
   return (
     <>
-      <Text strong>ペアリング数</Text>
-      <br />
-      <Space>{pairingCountsString}</Space>
+      <Text strong>ペア数</Text>
+      <Table dataSource={dataSource} columns={columns} pagination={false} />
     </>
   );
 };
