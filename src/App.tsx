ce2ffe056eb453adff3c542b7a2bfe3f3c52c080
KM0912 <61630275+KM0912@users.noreply.github.com>
2024-11-11 08:38:46 +0900

    Revert "基本的な機能を実装"

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import React from "react";
-import "./App.css";
-import MainComponent from "./components/pages/Main";
-import "tailwindcss/tailwind.css";
-import { PlayerProvider } from "./contexts/PlayerContext";
-import { MatchProvider } from "./contexts/MatchContext";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <PlayerProvider>
-        <MatchProvider>
-          <MainComponent />
-        </MatchProvider>
-      </PlayerProvider>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
