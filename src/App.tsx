b82500ddcbb5539f1497e5db36a286de8898b8b4
KM0912 <61630275+KM0912@users.noreply.github.com>
2024-11-14 07:38:57 +0900

    Google AnalyticsのトラッキングIDを環境変数から取得し、Appコンポーネントで動的にスクリプトを追加する機能を実装 .env.sampleファイルを追加し、.envを.gitignoreに追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import MainComponent from "./components/pages/Main";
 import "tailwindcss/tailwind.css";
@@ -6,6 +6,26 @@ import { PlayerProvider } from "./contexts/PlayerContext";
 import { MatchProvider } from "./contexts/MatchContext";
 
 function App() {
+  useEffect(() => {
+    const trackingId = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID;
+
+    // Google Analytics スクリプトを動的に追加
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${trackingId}`;
+    document.head.appendChild(script);
+
+    // Google Analytics 設定用スクリプトを追加
+    const inlineScript = document.createElement("script");
+    inlineScript.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${trackingId}');
+    `;
+    document.head.appendChild(inlineScript);
+  }, []);
+
   return (
     <div className="App">
       <PlayerProvider>
